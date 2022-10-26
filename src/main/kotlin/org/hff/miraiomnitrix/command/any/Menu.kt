46a152916b00b51a90c3e645b90d1e0e1dd81863
null <975959057@qq.com>
2022-10-26 18:19:00 +0800

    update

--- src/main/kotlin/org/hff/miraiomnitrix/command/any/Menu.kt
+++ src/main/kotlin/org/hff/miraiomnitrix/command/any/Menu.kt
@@ -3,7 +3,6 @@ package org.hff.miraiomnitrix.command.any
 import net.mamoe.mirai.contact.Contact
 import net.mamoe.mirai.contact.User
 import net.mamoe.mirai.message.data.MessageChain
-import net.mamoe.mirai.message.data.buildMessageChain
 import org.hff.miraiomnitrix.command.Command
 import org.hff.miraiomnitrix.result.ResultMessage
 import org.hff.miraiomnitrix.result.result
@@ -17,7 +16,8 @@ class Menu : AnyCommand {
         |2: 壁纸、bizhi，可追加:精选、横屏(pc)、竖屏(mp)、银发、兽耳、涩图
         |3: bgm，可追加:年份(4位数字) r[最低排名] n[数量] 任意关键词
         |4: 搜图、soutu、st，请在关键字后面粘贴一张图片
-        |5: js，执行js程序
+        |5: js: 执行js程序
+        |6: 复读、echo: 复读后面的文字
     """.trimMargin()
 
     override suspend fun execute(
