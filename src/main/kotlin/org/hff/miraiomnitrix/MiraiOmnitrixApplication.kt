e7c798a3956b515289e8f4a7c756851508c37d34
null <975959057@qq.com>
2023-02-09 16:50:29 +0800

    update

--- src/main/kotlin/org/hff/miraiomnitrix/MiraiOmnitrixApplication.kt
+++ src/main/kotlin/org/hff/miraiomnitrix/MiraiOmnitrixApplication.kt
@@ -4,9 +4,8 @@ import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.EnableAspectJAutoProxy
 
-@EnableAspectJAutoProxy
+//@EnableAspectJAutoProxy
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @MapperScan("org.hff.miraiomnitrix.app.mapper")
