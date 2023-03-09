e82a48273f74aed7a6a55a4758d9b282fbf592c5
null <975959057@qq.com>
2023-03-09 19:01:46 +0800

    update

--- src/main/kotlin/org/hff/miraiomnitrix/event/any/Search.kt
+++ src/main/kotlin/org/hff/miraiomnitrix/event/any/Search.kt
@@ -1,5 +1,6 @@
 package org.hff.miraiomnitrix.event.any
 
+import net.mamoe.mirai.contact.Contact
 import net.mamoe.mirai.contact.Contact.Companion.uploadImage
 import net.mamoe.mirai.event.events.MessageEvent
 import net.mamoe.mirai.message.data.Image
@@ -11,6 +12,7 @@ import org.hff.miraiomnitrix.event.*
 import org.hff.miraiomnitrix.utils.HttpUtil
 import org.hff.miraiomnitrix.utils.JsonUtil
 import org.hff.miraiomnitrix.utils.getInfo
+import org.jsoup.Jsoup
 
 @Event(priority = 4)
 class Search(private val accountProperties: AccountProperties) : AnyEvent {
@@ -20,24 +22,39 @@ class Search(private val accountProperties: AccountProperties) : AnyEvent {
     override suspend fun handle(args: List<String>, event: MessageEvent): EventResult {
         if (args.isEmpty()) return next()
         if (!commands.any { it in args }) return next()
-        val saucenaoKey = accountProperties.saucenaoKey ?: return stop("未配置SauceNAO的Key")
 
         val (subject, _, message) = event.getInfo()
 
         val quote = message[QuoteReply.Key]
-        val image = if (quote != null) {
+        val imgUrl = if (quote != null) {
             val imageId = Cache.imageCache.getIfPresent(quote.source.internalIds[0]) ?: return next()
-            Image(imageId)
-        } else message[Image.Key] ?: return next()
+            Image(imageId).queryUrl()
+        } else message[Image.Key]?.queryUrl() ?: return next()
 
-        val json = HttpUtil.getStringByProxy("$url$saucenaoKey&url=${image.queryUrl()}")
-        val results: List<Result> = JsonUtil.fromJson(json, "results")
-        if (results.isEmpty()) return stop()
+        trySauceNAO(imgUrl, subject)
+
+        return stop()
+    }
+
+    suspend fun trySauceNAO(imgUrl: String, subject: Contact): Boolean {
+        val saucenaoKey = accountProperties.saucenaoKey ?: return false
+        val json = HttpUtil.getStringByProxy("$url$saucenaoKey&url=$imgUrl")
+        val results: List<SauceNAOResult> = JsonUtil.fromJson(json, "results")
+        if (results.isEmpty()) {
+            subject.sendMessage("SauceNAO未找到搜图结果")
+            return false
+        }
         val (header, data) = results[0]
         val urls = data.ext_urls
-        if (urls.isNullOrEmpty()) return stop("无搜图结果")
+        if (urls.isNullOrEmpty()) {
+            subject.sendMessage("SauceNAO未找到搜图结果")
+            return false
+        }
         val similarity = header.similarity.toDouble()
-        if (similarity < 60) return stop("相似度过低")
+        if (similarity < 60) {
+            subject.sendMessage("SauceNAO搜图结果相似度过低，切换下一个引擎")
+            return false
+        }
         val thumbnailImg = HttpUtil.getInputStreamByProxy(header.thumbnail)
         val urlsText = if (urls.size == 1) {
             "链接: " + urls[0].trim('"')
@@ -45,17 +62,47 @@ class Search(private val accountProperties: AccountProperties) : AnyEvent {
             urls.mapIndexed { index, url -> "链接${index + 1}：${url.trim('"')}" }.joinToString("\n")
         }
         buildMessageChain {
-            +"搜图结果：\n"
+            +"SauceNAO搜图结果：\n"
             +subject.uploadImage(thumbnailImg)
             +"相似度：$similarity\n"
             if (data.title != null) +"标题：${data.title}\n"
             +"$urlsText\n"
             val author = data.member_name ?: data.user_name ?: data.creator ?: data.jp_name
             if (author != null) +"作者：$author"
-        }.apply { return stop(this) }
+        }.apply { return true }
     }
 
-    data class Result(
+    // 使用图片url请求ascii2d网站的搜图数据，解析第一条的html数据
+    suspend fun ascii2d(imgUrl: String, subject: Contact): Boolean {
+        val html = HttpUtil.getStringByProxy("https://ascii2d.net/search/url/$imgUrl")
+        val doc = Jsoup.parse(html)
+        val elements = doc.select("div[class=card-body]")
+        if (elements.size < 2) {
+            subject.sendMessage("ascii2d未找到搜图结果")
+            return false
+        }
+        val element = elements[1]
+        val similarity = element.select("div[class=card-text]").text().split(" ")[1].toDouble()
+        if (similarity < 60) {
+            subject.sendMessage("ascii2d搜图结果相似度过低，切换下一个引擎")
+            return false
+        }
+        val thumbnailImg = HttpUtil.getInputStreamByProxy(element.select("img").attr("src"))
+        val urls = element.select("a").map { it.attr("href") }
+        val urlsText = if (urls.size == 1) {
+            "链接: " + urls[0].trim('"')
+        } else {
+            urls.mapIndexed { index, url -> "链接${index + 1}：${url.trim('"')}" }.joinToString("\n")
+        }
+        buildMessageChain {
+            +"ascii2d搜图结果：\n"
+            +subject.uploadImage(thumbnailImg)
+            +"相似度：$similarity\n"
+            +"$urlsText\n"
+        }.apply { return true }
+    }
+
+    data class SauceNAOResult(
         val header: Header,
         val data: Data
     )
